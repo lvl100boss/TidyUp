b3b1c5e3699a5cd00f8e65f3ab89a1f7ae0a492e
Art Art <144819984+lvl100boss@users.noreply.github.com>
2025-02-11 22:16:50 +0800

    Made a Route for all the admin pages

--- routes/admin.php
+++ routes/admin.php
@@ -15,6 +15,18 @@ Route::get('/admin/users', function () {
     return Inertia::render('Admin/Users');
 })->middleware(['auth', 'verified'])->name('admin.users');
 
+Route::get('/admin/analytics', function () {
+    return Inertia::render('Admin/Analytics');
+})->middleware(['auth', 'verified'])->name('admin.analytics');
+
+Route::get('/admin/feedbacks', function () {
+    return Inertia::render('Admin/UserFeedback');
+})->middleware(['auth', 'verified'])->name('admin.feedbacks');
+
+Route::get('/admin/customer-service', function () {
+    return Inertia::render('Admin/CustomerService');
+})->middleware(['auth', 'verified'])->name('admin.customer-service');
+
 Route::get('/admin/restriction', function () {
     return Inertia::render('Admin/Restriction');
 })->middleware(['auth', 'verified'])->name('admin.restriction');
