bbb1d5df76da5bd20a982bdd8878d86b2a4e7b6c
Art Art <144819984+lvl100boss@users.noreply.github.com>
2025-02-11 22:09:22 +0800

    Shops Route for admin

--- resources/js/Components/Admin/sidebar.jsx
+++ resources/js/Components/Admin/sidebar.jsx
@@ -17,7 +17,7 @@ import {
 export default function Sidebar() {
     const navItems = [
         { label: "Dashboard", href: "/admin/dashboard", icon: Home },
-        { label: "Shops", href: "/admin/users", icon: Store },
+        { label: "Shops", href: "/admin/shops", icon: Store },
         { label: "Users", href: "/admin/users", icon: UsersRound },
         { label: "Appointments", href: "/admin/reports", icon: Calendar },
         { label: "Sales Report", href: "/admin/content", icon: Tag },
