d09ef72da8acb506b318f6e5687a3f74fec4b61f
Art Art <144819984+lvl100boss@users.noreply.github.com>
2025-02-11 21:40:07 +0800

    Users Route for Admin

--- resources/js/Components/Admin/sidebar.jsx
+++ resources/js/Components/Admin/sidebar.jsx
@@ -18,7 +18,7 @@ export default function Sidebar() {
     const navItems = [
         { label: "Dashboard", href: "/admin/dashboard", icon: Home },
         { label: "Shops", href: "/admin/users", icon: Store },
-        { label: "Customers", href: "/admin/orders", icon: UsersRound },
+        { label: "Users", href: "/admin/users", icon: UsersRound },
         { label: "Appointments", href: "/admin/reports", icon: Calendar },
         { label: "Sales Report", href: "/admin/content", icon: Tag },
         { label: "Invoice", href: "/admin/settings", icon: Receipt },
