cc373b9801293e96ea9d7903320cb6da7d751013
Art Art <144819984+lvl100boss@users.noreply.github.com>
2025-02-11 21:12:32 +0800

    restriction page route

---
+++ resources/js/Pages/Admin/Restriction.jsx
@@ -0,0 +1,14 @@
+import AdminLayout from "@/Layouts/AdminLayout";
+import { Head } from "@inertiajs/react";
+
+export default function Restriction() {
+    return (
+        <AdminLayout>
+            <Head title="Restriction" />
+            <div>
+                {/* Codes Here */}
+                <h1 className="">This is Restriction Page</h1>
+            </div>
+        </AdminLayout>
+    );
+}
