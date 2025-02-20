acd1fef06ec42b4c80b35b29daa04434a6763ab3
Art Art <144819984+lvl100boss@users.noreply.github.com>
2025-02-21 07:33:29 +0800

    i added Popular page, barbershop page and hairsalon page

---
+++ app/Http/Controllers/BarbershopShopsController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use App\Models\Shop;
+
+class BarbershopShopsController extends Controller
+{
+    //
+    public function index()
+    {
+        $shops = Shop::with(['shopGallery', 'shopCategories' => function ($query) {
+            $query->whereHas('categories', function ($query) {
+                $query->where('name', 'Barbershop');
+            });
+        }, 'shopCategories.categories'])->whereHas('shopCategories.categories', function ($query) {
+            $query->where('name', 'Barbershop');
+        })->inRandomOrder()->limit(20)->get();
+
+        return Inertia::render(
+            'Users/Barbershops',
+            [
+                'shops' => $shops,
+            ]
+        );
+    }
+}
