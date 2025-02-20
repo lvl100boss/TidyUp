6e8f547959c045a51c9c7c5a1915ad15a82cb17d
Dave Jamir Basa <basajamir@gmail.com>
2025-02-20 22:45:50 +0800

    Hello

--- routes/admin.php
+++ routes/admin.php
@@ -6,7 +6,7 @@ use Inertia\Inertia;
 Route::get('/admin/dashboard', function () {
     return Inertia::render('Admin/Dashboard');
 })->middleware(['auth', 'verified'])->name('admin.dashboard');
-Route::redirect('/admin', '/admin/dashboard')->middleware(['auth', 'verified']);
+Route::redirect('/admin', '/badmin/dashboard')->middleware(['auth', 'verified']);
 
 Route::get('/admin/shops', function () {
     return Inertia::render('Admin/Shops');
