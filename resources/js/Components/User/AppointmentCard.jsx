f61a9287f8d5d54e46371375b93ff057e8a7ec02
pauldaniel25 <pauldanielojales@gmail.com>
2025-05-05 02:08:07 +0800

    Update AppointmentCard.jsx

--- resources/js/Components/User/AppointmentCard.jsx
+++ resources/js/Components/User/AppointmentCard.jsx
@@ -9,7 +9,7 @@ const AppointmentCard = ({ appointment }) => {
         month: "short",
         day: "2-digit",
     });
-    const time = new Date(`1970-01-01T${appointment.time}Z`).toLocaleTimeString(
+    const time = new Date(`1970-01-01T${appointment.time}`).toLocaleTimeString(
         "en-US",
         {
             hour: "2-digit",
